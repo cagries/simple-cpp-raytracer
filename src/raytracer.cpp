6006bac16d1c1a234bade34a7b2cc663467bf6c6
dorukcoskun <1dorukcoskun@gmail.com>
2018-10-26 18:08:35 +0300

    Shadows no Love

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -41,11 +41,13 @@ Vec3f RayTracer::calculateColor(Ray viewRay, Vec3f positionColor ,int recursionL
                 hitFlag = true;
             }
         }
+        
         for (size_t i = 0; i < scene.spheres.size(); i++) {
             if (scene.spheres[i].hit(viewRay, &hr)) {
                 hitFlag = true;
             }
         }
+        
         for (size_t i = 0; i < scene.meshes.size(); i++) {
             for (auto& triangle : scene.meshes[i].faces) {
                 if (triangle.hit(viewRay, &hr)) {
@@ -90,19 +92,41 @@ Vec3f RayTracer::calculateEachLight(HitRecord hr, PointLight light, Vec3f viewVe
         return color;
     }
 
-    
     //Shadows
-/*    for (size_t i = 0; i < scene.spheres.size(); i++) {
-        if (scene.spheres[i].hit(lightRay, nullptr)) {
-            return color;
+    HitRecord shadowRec;
+    bool shadowHitFlag = false;
+    for (size_t i = 0; i < scene.spheres.size(); i++) {
+        if (scene.spheres[i].hit(lightRay, &shadowRec)) {
+            shadowHitFlag = true;
         }
     }
-*/
+    
+    for (size_t i = 0; i < scene.triangles.size(); i++) {
+        if (scene.triangles[i].hit(lightRay, &shadowRec)) {
+            shadowHitFlag = true;
+        }
+    }
+    
+    for (size_t i = 0; i < scene.meshes.size(); i++) {
+        for (auto& triangle : scene.meshes[i].faces) {
+            if (triangle.hit(lightRay, &shadowRec)) {
+                shadowHitFlag = true;
+            }
+        }
+    }
+    
+    if (shadowHitFlag && (shadowRec.pos - hr.pos).norm() < distance) {
+        return color;
+    }
+    
+    //Calculate Diffuse Shading
     color += (1 / (distance * distance)) * dot * (hr.m->diffuse.times(light.intensity));
     
     //Calculations of half vector.
     Vec3f h = (lightVector + -viewVector);
     h = h / h.norm();
+    
+    //Calculate Blinn-Phong Shader
     color += (1 / (distance * distance)) * pow((std::max(0.0f, h * hr.normal)), hr.m->phong_exponent) * hr.m->specular.times(light.intensity);
 
     return color;
